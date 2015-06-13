1bdc1da1d19feec9a4052982a8e49e0291f77212
Sergey Levine <svlevine@stanford.edu>
2015-06-12 23:29:09 -0700

    starting on plugin object

---
+++ src/agent/controller/pr2plugin.cpp
@@ -0,0 +1,76 @@
+#include "agent/controller/pr2plugin.h"
+
+using namespace gps_control;
+
+// Plugin constructor.
+pr2_plugin::pr2_plugin()
+{
+    // Nothing to do here, since all variables are initialized in initialize(...)
+}
+
+// Destructor.
+void pr2_plugin::~pr2_plugin()
+{
+    // Nothing to do here, since all instance variables are destructed automatically.
+}
+
+// Initialize the object and store the robot state.
+bool pr2_plugin::init(pr2_mechanism_model::RobotState* robot, ros::NodeHandle& n)
+{
+    // Variables.
+    std::string root_name, active_tip_name, passive_tip_name;
+
+    // Initialize ROS subscribers/publishers, sensors, and position controllers.
+    initialize(n);
+
+    // Store the robot state.
+    robot_ = robot;
+
+    // Create FK solvers.
+    // Get the name of the root.
+    if(!n.getParam("root_name", root_name)) {
+        ROS_ERROR("Property root_name not found in namespace: '%s'", n.getNamespace().c_str());
+        return false;
+    }
+
+    // Get active and passive arm end-effector names.
+    if(!n.getParam("active_tip_name", active_tip_name)) {
+        ROS_ERROR("Property active_tip_name not found in namespace: '%s'", n.getNamespace().c_str());
+        return false;
+    }
+    if(!n.getParam("passive_tip_name", passive_tip_name)) {
+        ROS_ERROR("Property passive_tip_name not found in namespace: '%s'", n.getNamespace().c_str());
+        return false;
+    }
+
+    // Create active arm chain.
+    if(!active_arm_chain_.init(robot_, root_name, active_tip_name)) {
+        ROS_ERROR("Controller could not use the chain from '%s' to '%s'", root_name.c_str(), active_tip_name.c_str());
+        return false;
+    }
+
+    // Create passive arm chain.
+    if(!passive_arm_chain_.init(robot_, root_name, passive_tip_name)) {
+        ROS_ERROR("Controller could not use the chain from '%s' to '%s'", root_name.c_str(), passive_tip_name.c_str());
+        return false;
+    }
+
+    // Create KDL chains, solvers, etc.
+    // KDL chains.
+    passive_arm_chain_.toKDL(passive_arm_fk_chain_);
+    active_arm_chain_.toKDL(active_arm_fk_chain_);
+
+    // Pose solvers.
+    passive_arm_fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(passive_arm_fk_chain_));
+    active_arm_fk_solver_.reset(new KDL::ChainFkSolverPos_recursive(active_arm_fk_chain_));
+
+    // Jacobian sovlers.
+    passive_arm_jac_solver_.reset(new KDL::ChainJntToJacSolver(passive_arm_fk_chain_));
+    active_arm_jac_solver_.reset(new KDL::ChainJntToJacSolver(active_arm_fk_chain_));
+
+    // Tell the PR2 controller manager that we initialized everything successfully.
+    return true;
+}
+
+// 
+
---
+++ src/agent/controller/robotplugin.cpp
@@ -0,0 +1,73 @@
+#include "agent/controller/robotplugin.h"
+#include "agent/controller/sensor.h"
+#include "agent/controller/controller.h"
+#include "agent/controller/positioncontroller.h"
+
+using namespace gps_control;
+
+// Plugin constructor.
+robot_plugin::robot_plugin()
+{
+    // Nothing to do here, since all variables are initialized in initialize(...)
+}
+
+// Destructor.
+void robot_plugin::~robot_plugin()
+{
+    // Nothing to do here, since all instance variables are destructed automatically.
+}
+
+// Initialize everything.
+void robot_plugin::initialize(ros::NodeHandle& n)
+{
+    // Initialize all ROS communication infrastructure.
+    initialize_ros(n);
+
+    // Initialize all sensors.
+    initialize_sensors(n);
+
+    // Initialize the position controllers.
+    // Note that the trial controllers are created from scratch for each trial.
+    // However, the position controllers persist, since there is only one type.
+    initialize_position_controllers(n);
+
+    // After this, we still need to create the kinematics solvers. How these are
+    // created depends on the particular robot, and should be implemented in a
+    // subclass.
+}
+
+// Initialize ROS communication infrastructure.
+void robot_plugin::initialize_ros(ros::NodeHandle& n)
+{
+    // Create subscribers.
+    position_subscriber_ = n.subscribe("/gps_controller_position_command", 1, &robot_plugin::position_subscriber_callback, this);
+    trial_subscriber_ = n.subscribe("/gps_controller_trial_command", 1, &robot_plugin::trial_subscriber_callback, this);
+    relax_subscriber_ = n.subscribe("/gps_controller_relax_command", 1, &robot_plugin::relax_subscriber_callback, this);
+    report_subscriber_ = n.subscribe("/gps_controller_report_command", 1, &robot_plugin::report_subscriber_callback, this);
+
+    // Create publishers.
+    report_publisher_.reset(new realtime_tools::RealtimePublisher<report_msg>(n, "/gps_controller_report", 1));
+}
+
+// Initialize all sensors.
+void robot_plugin::initialize_sensors(ros::NodeHandle& n)
+{
+    // Clear out the old sensors.
+    sensors_.clear();
+
+    // Create all sensors.
+    for (sensor_type i = 0; i < sensor_type::total_sensor_types; i++)
+    {
+        sensors_.push_back(sensor::create_sensor(i,n,this));
+    }
+}
+
+// Initialize position controllers.
+void robot_plugin::initialize_position_controllers(n)
+{
+    // Create passive arm position controller.
+    passive_arm_controller_.reset(new position_controller(n));
+
+    // Create active arm position controller.
+    active_arm_controller_.reset(new position_controller(n));
+}
