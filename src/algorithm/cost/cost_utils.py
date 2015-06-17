83410afeaf695da36e3329e15fe731a62b42b5f9
Justin Fu <justin.fu@reflektion.com>
2015-06-16 17:47:10 -0700

    Remove transposes in evall1l2/evallogl2

--- src/algorithm/cost/cost_utils.py
+++ src/algorithm/cost/cost_utils.py
@@ -181,32 +181,25 @@ def evall1l2term(wp, d, Jd, Jdd, l1, l2, alpha):
     Evaluate and compute derivatives for combined l1/l2 norm penalty.
 
     Args:
-        wp: 
-            TxD matrix containing weights for each dimension and timestep
+        wp:
+            T x D matrix containing weights for each dimension and timestep
         d:
-            TxD
+            T x D
         Jd:
-            TxDxD
+            T x Dx x D
         Jdd:
-            TxDxDxD
+            T x Dx x Dx x D
         l1: l1 loss weight
         l2: l2 loss weight
         alpha:
 
     Returns:
-        l: Tx1 Evaluated loss
-        lx: TxD First derivative
-        lxx: TxDxD Second derivative
+        l: T x 1 Evaluated loss
+        lx: T x Dx First derivative
+        lxx: T x Dx x Dx Second derivative
     """
-    # Evaluate a combined L1/L2 norm penalty.
-    # TODO: Don't transpose everything in the beginning to match the matlab code
-    d = d.T
-    wp = wp.T
-    Jd = np.transpose(Jd, [1, 2, 0])
-    Jdd = np.transpose(Jdd, [1, 2, 3, 0])
-
     # Get trajectory length.
-    _, T = d.shape
+    T, _ = d.shape
 
     # Compute scaled quantities.
     sqrtwp = np.sqrt(wp)
@@ -215,47 +208,36 @@ def evall1l2term(wp, d, Jd, Jdd, l1, l2, alpha):
     dscls = d * (wp ** 2)
 
     # Compute total cost.
-    l = 0.5 * np.sum(dsclsq ** 2, axis=0, keepdims=True) * l2 \
-        + np.sqrt(alpha + np.sum(dscl ** 2, axis=0, keepdims=True)) * l1
+    l = 0.5 * np.sum(dsclsq ** 2, axis=1, keepdims=True) * l2 \
+        + np.sqrt(alpha + np.sum(dscl ** 2, axis=1, keepdims=True)) * l1
 
     # First order derivative terms.
-    d1 = dscl * l2 + (dscls / np.sqrt(alpha + np.sum(dscl ** 2, axis=0, keepdims=True)) * l1)
-    lx = np.transpose(np.sum(Jd * np.expand_dims(d1, axis=0), axis=1, keepdims=True), [0, 2, 1])
-    assert lx.shape[2] == 1
-    lx = lx[:, :, 0]
+    d1 = dscl * l2 + (dscls / np.sqrt(alpha + np.sum(dscl ** 2, axis=1, keepdims=True)) * l1)
+    lx = np.sum(Jd * np.expand_dims(d1, axis=1), axis=2)
 
     # Second order terms.
-    psq = np.expand_dims(np.sqrt(alpha + np.sum(dscl ** 2, axis=0, keepdims=True)), axis=0)
-    d2 = l1 * ((np.expand_dims(np.eye(wp.shape[0]), axis=2) * (np.expand_dims(wp ** 2, axis=1) / psq)) -
-               ((np.expand_dims(dscls, axis=1) * np.expand_dims(dscls, axis=0)) / psq ** 3))
-    d2 += l2 * (np.expand_dims(wp, axis=1) * np.transpose(np.tile(np.eye(wp.shape[0]), [T, 1, 1]), [1, 2, 0]))
+    psq = np.expand_dims(np.sqrt(alpha + np.sum(dscl ** 2, axis=1, keepdims=True)), axis=1)
+    d2 = l1 * ((np.expand_dims(np.eye(wp.shape[1]), axis=0) * (np.expand_dims(wp ** 2, axis=1) / psq)) -
+               ((np.expand_dims(dscls, axis=1) * np.expand_dims(dscls, axis=2)) / psq ** 3))
+    d2 += l2 * (np.expand_dims(wp, axis=2) * np.tile(np.eye(wp.shape[1]), [T, 1, 1]))
 
-    d1_expand = np.expand_dims(np.expand_dims(d1.T, axis=0), axis=0)
-    sec = np.sum(d1_expand * np.transpose(Jdd, [0, 1, 3, 2]), axis=3)
+    d1_expand = np.expand_dims(np.expand_dims(d1, axis=-1), axis=-1)
+    sec = np.sum(d1_expand * np.transpose(Jdd, [0, 3, 1, 2]), axis=1)
 
-    Jd_expand_1 = np.expand_dims(np.expand_dims(np.transpose(Jd, [0, 2, 1]), axis=1), axis=4)
-    Jd_expand_2 = np.expand_dims(np.expand_dims(np.transpose(Jd, [0, 2, 1]), axis=2), axis=0)
-    d2_expand = np.expand_dims(np.expand_dims(np.transpose(d2, [2, 0, 1]), axis=0), axis=0)
-    lxx = np.sum(np.sum((Jd_expand_1 * Jd_expand_2) * d2_expand, axis=3, keepdims=True), axis=4, keepdims=True)
-    assert lxx.shape[3:5] == (1, 1)
-    lxx = lxx[:, :, :, 0, 0]
+    Jd_expand_1 = np.expand_dims(np.expand_dims(Jd, axis=2), axis=4)
+    Jd_expand_2 = np.expand_dims(np.expand_dims(Jd, axis=1), axis=3)
+    d2_expand = np.expand_dims(np.expand_dims(d2, axis=1), axis=1)
+    lxx = np.sum(np.sum((Jd_expand_1 * Jd_expand_2) * d2_expand, axis=-1), axis=-1)
 
-    lxx += 0.5 * sec + 0.5 * np.transpose(sec, [1, 0, 2])
+    lxx += 0.5 * sec + 0.5 * np.transpose(sec, [0,2,1])
 
-    import pdb; pdb.set_trace()
-    return l.T, lx.T, np.transpose(lxx, [2, 0, 1])
+    return l, lx, lxx
 
 
 def evallogl2term(wp, d, Jd, Jdd, l1, l2, alpha):
     """ Evaluate and compute derivatives for combined l1/l2 norm penalty. """
-    # TODO: Don't transpose everything in the beginning to match the matlab code
-    d = d.T
-    wp = wp.T
-    Jd = np.transpose(Jd, [1, 2, 0])
-    Jdd = np.transpose(Jdd, [1, 2, 3, 0])
-
     # Get trajectory length.
-    _, T = d.shape
+    T, _ = d.shape
 
     # Compute scaled quantities.
     sqrtwp = np.sqrt(wp)
@@ -264,31 +246,27 @@ def evallogl2term(wp, d, Jd, Jdd, l1, l2, alpha):
     dscls = d * (wp ** 2)
 
     # Compute total cost.
-    l = 0.5 * np.sum(dsclsq ** 2, axis=0, keepdims=True) * l2 \
-        + 0.5*np.log(alpha + np.sum(dscl ** 2, axis=0, keepdims=True)) * l1
+    l = 0.5 * np.sum(dsclsq ** 2, axis=1, keepdims=True) * l2 \
+        + 0.5*np.log(alpha + np.sum(dscl ** 2, axis=1, keepdims=True)) * l1
 
     # First order derivative terms.
-    d1 = dscl * l2 + (dscls / (alpha + np.sum(dscl ** 2, axis=0, keepdims=True)) * l1)
-    lx = np.transpose(np.sum(Jd * np.expand_dims(d1, axis=0), axis=1, keepdims=True), [0, 2, 1])
-    assert lx.shape[2] == 1
-    lx = lx[:, :, 0]
+    d1 = dscl * l2 + (dscls / (alpha + np.sum(dscl ** 2, axis=1, keepdims=True)) * l1)
+    lx = np.sum(Jd * np.expand_dims(d1, axis=1), axis=2)
 
     # Second order terms.
-    psq = np.expand_dims((alpha + np.sum(dscl ** 2, axis=0, keepdims=True)), axis=0)
-    d2 = l1 * ((np.expand_dims(np.eye(wp.shape[0]), axis=2) * (np.expand_dims(wp ** 2, axis=1) / psq)) -
-               ((np.expand_dims(dscls, axis=1) * np.expand_dims(dscls, axis=0)) / psq ** 2))
-    d2 += l2 * (np.expand_dims(wp, axis=1) * np.transpose(np.tile(np.eye(wp.shape[0]), [T, 1, 1]), [1, 2, 0]))
+    psq = np.expand_dims((alpha + np.sum(dscl ** 2, axis=1, keepdims=True)), axis=1)
+    d2 = l1 * ((np.expand_dims(np.eye(wp.shape[1]), axis=0) * (np.expand_dims(wp ** 2, axis=1) / psq)) -
+               ((np.expand_dims(dscls, axis=1) * np.expand_dims(dscls, axis=2)) / psq ** 2))
+    d2 += l2 * (np.expand_dims(wp, axis=2) * np.tile(np.eye(wp.shape[1]), [T, 1, 1]))
 
-    d1_expand = np.expand_dims(np.expand_dims(d1.T, axis=0), axis=0)
-    sec = np.sum(d1_expand * np.transpose(Jdd, [0, 1, 3, 2]), axis=3)
+    d1_expand = np.expand_dims(np.expand_dims(d1, axis=-1), axis=-1)
+    sec = np.sum(d1_expand * np.transpose(Jdd, [0, 3, 1, 2]), axis=1)
 
-    Jd_expand_1 = np.expand_dims(np.expand_dims(np.transpose(Jd, [0, 2, 1]), axis=1), axis=4)
-    Jd_expand_2 = np.expand_dims(np.expand_dims(np.transpose(Jd, [0, 2, 1]), axis=2), axis=0)
-    d2_expand = np.expand_dims(np.expand_dims(np.transpose(d2, [2, 0, 1]), axis=0), axis=0)
-    lxx = np.sum(np.sum((Jd_expand_1 * Jd_expand_2) * d2_expand, axis=3, keepdims=True), axis=4, keepdims=True)
-    assert lxx.shape[3:5] == (1, 1)
-    lxx = lxx[:, :, :, 0, 0]
+    Jd_expand_1 = np.expand_dims(np.expand_dims(Jd, axis=2), axis=4)
+    Jd_expand_2 = np.expand_dims(np.expand_dims(Jd, axis=1), axis=3)
+    d2_expand = np.expand_dims(np.expand_dims(d2, axis=1), axis=1)
+    lxx = np.sum(np.sum((Jd_expand_1 * Jd_expand_2) * d2_expand, axis=-1), axis=-1)
 
-    lxx += 0.5 * sec + 0.5 * np.transpose(sec, [1, 0, 2])
+    lxx += 0.5 * sec + 0.5 * np.transpose(sec, [0,2,1])
 
-    return l.T, lx.T, np.transpose(lxx, [2, 0, 1])
+    return l, lx, lxx
