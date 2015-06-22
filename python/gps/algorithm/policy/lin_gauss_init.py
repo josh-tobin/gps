9525a0c62449009be7707d572065fc501c518d83
Justin Fu <justin.fu@reflektion.com>
2015-06-21 23:06:04 -0700

    Verify init_lqr, add more comments

--- python/gps/algorithm/policy/lin_gauss_init.py
+++ python/gps/algorithm/policy/lin_gauss_init.py
@@ -7,9 +7,8 @@ from algorithm.dynamics.dynamics_util import guess_dynamics
 
 def init_lqr(x0, dX, dU, dt, T, gains, acc, init_stiffness, init_stiffness_vel, init_var, init_final_weight):
     """
-    Return initial gains for a time-varying linear gaussian controller.
-
-    *** This code runs, but returns incorrect results.
+    Return initial gains for a time-varying linear gaussian controller
+    that tries to hold the initial position.
 
     Some sanity checks
     >>> x0 = np.zeros(8)
@@ -18,54 +17,63 @@ def init_lqr(x0, dX, dU, dt, T, gains, acc, init_stiffness, init_stiffness_vel, 
     (5, 3, 8)
     """
     #TODO: Use packing instead of assuming which indices are the joint angles.
-    #TODO: Comment on variables.
     ref = np.hstack([np.tile(x0, [T, 1]), np.zeros((T, dU))])
 
+    # Notation notes:
+    # L = loss, Q = q-function (dX+dU dimensional), V = value function (dX dimensional), F = dynamics
+    # Vectors are lower-case, Matrices are upper case
+    # Derivatives: x = state, u = action, t = state+action (trajectory). All lowercase
+    # The time index is denoted by _t after the above.
+    # Ex. Ltt_t = Loss, 2nd derivative (w.r.t. trajectory), indexed by time t.
+
     # Constants.
-    ix = slice(dX)
-    iu = slice(dX, dX + dU)
+    idx_x = slice(dX)  # Slices out state
+    idx_u = slice(dX, dX + dU)  # Slices out actions
 
     # Set up simple linear dynamics model.
-    fd, fc = guess_dynamics(gains, acc, dX, dU, dt)
-    # Set up cost function.
-    Cm = np.diag(np.hstack([init_stiffness*np.ones(dU),
+    Fd, fc = guess_dynamics(gains, acc, dX, dU, dt)
+
+    # Setup a cost function based on stiffness.
+    # Ltt = (dX+dU) by (dX+dU) - second derivative of loss with respect to trajectory at a single timestep
+    Ltt = np.diag(np.hstack([init_stiffness*np.ones(dU),
                             init_stiffness*init_stiffness_vel*np.ones(dU),
                             np.zeros(dX-dU*2),
                             np.ones(dU)]))
-    Cm = Cm / init_var
-    cv = np.zeros(dX + dU)  # Derivative of cost
+    Ltt = Ltt / init_var
+    lt = np.zeros(dX + dU)  # lt = (dX+dU) - first derivative of loss with respect to trajectory at a single timestep
     # Perform dynamic programming.
-    K = np.zeros((dU, dX, T))
-    k = np.zeros((dU, T))
-    PSig = np.zeros((dU, dU, T))
-    cholPSig = np.zeros((dU, dU, T))
-    invPSig = np.zeros((dU, dU, T))
+    K = np.zeros((dU, dX, T))  # Controller gains matrix
+    k = np.zeros((dU, T))  # Controller bias term
+    PSig = np.zeros((dU, dU, T))  # Covariance of noise
+    cholPSig = np.zeros((dU, dU, T))  # Cholesky decomposition of covariance
+    invPSig = np.zeros((dU, dU, T))  # Inverse of covariance
     Vxx = np.zeros((dX, dX))  # Vxx = ddV/dXdX. Second deriv of value function at some timestep.
-    Vx = np.zeros(dX)  # Vx = dV/dX. Derivative of value function at some timestep.
+    vx = np.zeros(dX)  # Vx = dV/dX. Derivative of value function at some timestep.
     for t in range(T-1, -1, -1):
         # Compute Q function at this step.
-        if t == T:
-            Cmt = init_final_weight * Cm
-            cvt = init_final_weight * cv
+        if t == (T-1):
+            Ltt_t = init_final_weight * Ltt
+            lt_t = init_final_weight * lt
         else:
-            Cmt = Cm
-            cvt = cv
-        Qtt = Cmt + fd.T.dot(Vxx).dot(fd)
-        Qt = cvt + fd.T.dot(Vx + Vxx.dot(fc))
+            Ltt_t = Ltt
+            lt_t = lt
+        # Qtt = (dX+dU) by (dX+dU) 2nd Derivative of Q-function with respect to trajectory (dX+dU)
+        Qtt = Ltt_t + Fd.T.dot(Vxx).dot(Fd)
+        # Qt = (dX+dU) 1st Derivative of Q-function with respect to trajectory (dX+dU)
+        qt = lt_t + Fd.T.dot(vx + Vxx.dot(fc))
 
-        #TODO: There is incorrectness here (need to debug)
         # Compute preceding value function.
-        L = np.linalg.cholesky(Qtt[iu, iu])
-        invPSig[:, :, t] = Qtt[iu, iu]
+        L = np.linalg.cholesky(Qtt[idx_u, idx_u])
+        invPSig[:, :, t] = Qtt[idx_u, idx_u]
         PSig[:, :, t] = np.linalg.inv(L).dot(np.linalg.inv(L.T).dot(np.eye(dU)))
         cholPSig[:, :, t] = np.linalg.cholesky(PSig[:, :, t])
-        K[:, :, t] = -np.linalg.inv(L).dot(np.linalg.inv(L.T).dot(Qtt[iu, ix]))
-        k[:, t] = -np.linalg.inv(L).dot(np.linalg.inv(L.T).dot(Qt[iu]))
-        Vxx = Qtt[ix, ix] + Qtt[ix, iu].dot(K[:, :, t])
-        Vx = Qt[ix] + Qtt[ix, iu].dot(k[:, t])
+        K[:, :, t] = -np.linalg.inv(L).dot(np.linalg.inv(L.T).dot(Qtt[idx_u, idx_x]))
+        k[:, t] = -np.linalg.inv(L).dot(np.linalg.inv(L.T).dot(qt[idx_u]))
+        Vxx = Qtt[idx_x, idx_x] + Qtt[idx_x, idx_u].dot(K[:, :, t])
+        vx = qt[idx_x] + Qtt[idx_x, idx_u].dot(k[:, t])
         Vxx = 0.5 * (Vxx + Vxx.T)
 
-    #TODO: Remove tranposes once code is verified to be correct.
+    #TODO: Remove tranposes (artifact of porting over from matlab)
     K = np.transpose(K, [2, 0, 1])
     k = k.T
     PSig = np.transpose(PSig, [2, 0, 1])
@@ -76,11 +84,12 @@ def init_lqr(x0, dX, dU, dt, T, gains, acc, init_stiffness, init_stiffness_vel, 
 
 def init_pd(x0, dU, dQ, dX, T, init_stiffness, init_stiffness_vel, init_var, init_action_offset=None):
     """
-    Return initial gains for a time-varying linear gaussian controller.
+    Return initial gains for a time-varying linear gaussian controller that
+    tries to hold the initial position.
 
     Returns:
-        ref: T x dX+dU Reference trajectory + actions
-        K: T x dU x dX linear controller matrix
+        ref: T x dX+dU Reference trajectory
+        K: T x dU x dX linear controller gains matrix
         k: T x dU controller bias term
         PSig: T x dU x dU controller action covariance
         cholPSig: Cholesky decomposition of PSig
