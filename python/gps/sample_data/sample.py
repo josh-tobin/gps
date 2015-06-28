9282bf149859ba31af90e2ac13732dfae767cca3
Marvin Zhang <zhangmarvin95@gmail.com>
2015-06-28 15:34:14 -0700

    barebone MuJoCo simulator is almost functional now

--- python/gps/sample_data/sample.py
+++ python/gps/sample_data/sample.py
@@ -14,41 +14,51 @@ class Sample(object):
         self.T = sample_data.T
         self.dX = sample_data.dX
         self.dU = sample_data.dU
-        self.dObs = sample_data.dObs
+        self.dO = sample_data.dO
 
         # Dictionary containing the sample data from various sensors
         self._data = {}
 
-        self._X = np.empty((self.T, self.dX))
+        self._X = np.empty((self.dX, self.T))
         self._X.fill(np.nan)
-        self._obs = np.empty((self.T, self.dObs))
+        self._obs = np.empty((self.dO, self.T))
         self._obs.fill(np.nan)
 
-    def set(self, sensor_name, sensor_data):
+    def set(self, sensor_name, sensor_data, t=None):
         """Set trajectory data for a particular sensor"""
-        self._data[sensor_name] = sensor_data
-        self._X.fill(np.nan)  # Invalidate existing X
-        self._obs.fill(np.nan)  # Invalidate existing Obs
+        if t is None:
+            self._data[sensor_name] = sensor_data
+            self._X.fill(np.nan)  # Invalidate existing X
+            self._obs.fill(np.nan)  # Invalidate existing Obs
+        else:
+            if sensor_name not in self._data:
+                self._data[sensor_name] = np.empty((sensor_data.shape[0], self.T))
+                self._data[sensor_name].fill(np.nan)
+            self._data[sensor_name][:,t] = sensor_data
+            self._X[:,t].fill(np.nan)
+            self._obs[:,t].fill(np.nan)
 
-    def get(self, sensor_name):
+    def get(self, sensor_name, t=None):
         """Get trajectory data for a particular sensor"""
-        return self._data[sensor_name]
+        return self._data[sensor_name] if t is None else self._data[sensor_name][:,t]
 
-    def get_X(self):
+    def get_X(self, t=None):
         """Get the state. Put it together if not already precomputed."""
-        if np.any(np.isnan(self._X)):
+        X = self._X if t is None else self._X[:,t]
+        if np.any(np.isnan(X)):
             for data_type in self._data:
-                self.sample_data.pack_data_x(self._X, self._data[data_type], data_types=[data_type])
-        return self._X
+                self.sample_data.pack_data_x(X, self._data[data_type], data_types=[data_type], t=t)
+        return X
 
-    def get_U(self):
+    def get_U(self, t=None):
         """Get the action. Put it together if not already precomputed."""
-        return self._data[Action]
+        return self._data[Action] if t is None else self._data[Action][:,t]
 
-    def get_obs(self):
+    def get_obs(self, t=None):
         """Get the feature representation. Put it together if not already precomputed."""
-        if np.any(np.isnan(self._obs)):
+        obs = self._obs if t is None else self._obs[:,t]
+        if np.any(np.isnan(obs)):
             # TODO: Filter out data not in obs - Maybe this can be handled in pack_data_obs?
             for data_type in self._data:
-                self.sample_data.pack_data_obs(self._obs, self._data[data_type], data_types=[data_type])
-        return self._obs
+                self.sample_data.pack_data_obs(obs, self._data[data_type], data_types=[data_type], t=t)
+        return obs
