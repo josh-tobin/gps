536a0237142dc46119c14cdaa9782e638b379fd3
Marvin Zhang <zhangmarvin95@gmail.com>
2015-07-25 00:03:30 -0700

    multiple conditions work! at least, on the agent side...

--- python/gps/agent/mjc/agent_mjc.py
+++ python/gps/agent/mjc/agent_mjc.py
@@ -17,8 +17,24 @@ class AgentMuJoCo(Agent):
         config = deepcopy(agent_mujoco)
         config.update(hyperparams)
         Agent.__init__(self, config, sample_data)
+        self._setup_conditions()
         self._setup_world(hyperparams['filename'])
-        #TODO: add various other parameters, e.g. for drawing and appending to state
+
+    def _setup_conditions(self):
+        def setup(value, n):
+            if not isinstance(value, list):
+                try:
+                    return [value.copy() for _ in range(n)]
+                except AttributeError:
+                    return [value for _ in range(n)]
+            assert len(value) == n, 'number of elements must match number of conditions'
+            return value
+
+        #TODO: discuss a different way to organize some of this
+        conds = self._hyperparams['conditions']
+        for field in ('init_pose', 'x0var', 'pos_body_idx', 'pos_body_offset', \
+                'noisy_body_idx', 'noisy_body_var'):
+            self._hyperparams[field] = setup(self._hyperparams[field], conds)
 
     def _setup_world(self, filename):
         """
@@ -27,38 +43,48 @@ class AgentMuJoCo(Agent):
         Args:
             filename: path to XML file containing the world information
         """
-        self._world = mjcpy.MJCWorld2(filename)
+        self._world = mjcpy.MJCWorld(filename)
         options = {
             'timestep': self._hyperparams['dt']/self._hyperparams['substeps'],
-            'disableflags': 1 << 15,  # clearctrl
+            'disableflags': 0,
         }
         if self._hyperparams['rk']:
-            options['integrator'] = 3  # 3 = Runge-Katta
+            options['integrator'] = 3  # 3 = Runge-Kutta
         else:
             options['integrator'] = 2  # 2 = Semi-implicit Euler
-        self._world.SetOption(options)
-        self._model = self._world.GetModel()
-        self._data = self._world.GetData()
-        self._options = self._world.GetOption()
+        self._world.set_option(options)
 
-        #TODO: This is a hack to initialize end-effector sites. 
-        self.init_pose = self._hyperparams['init_pose']
-        x0 = np.r_[self.init_pose, np.zeros_like(self.init_pose)]
-        self.init_mj_X = x0
-        #TODO: the next line is bad and will break things if, e.g., gravity is on
-        self._world.Step(x0, np.zeros(self._model['nu']))
+        self._model = self._world.get_model()
+        self._model = [deepcopy(self._model) for _ in range(self._hyperparams['conditions'])]
+        self._options = self._world.get_option()
+
+        for i in range(self._hyperparams['conditions']):
+            for j in range(len(self._hyperparams['pos_body_idx'][i])):
+                idx = self._hyperparams['pos_body_idx'][i][j]
+                self._model[i]['body_pos'][idx,:] += self._hyperparams['pos_body_offset'][i]
+
+        self.init_mj_X = []
+        for init_pose in self._hyperparams['init_pose']:
+            #TODO: perhaps we might want to initialize velocities to something other than zeros?
+            x0 = np.r_[init_pose, np.zeros_like(init_pose)]
+            self.init_mj_X.append(x0)
+
+        #TODO: the next line is bad and can be potentially bad if, e.g., gravity is on
+        #      it only exists to initialize end effector sites
+        self._world.step(x0, np.zeros(self._model[0]['nu']))
 
         # Initialize x0
-        self._data = self._world.GetData()
+        self._data = self._world.get_data()
         eepts = self._data['site_xpos'].flatten()
 
-        # TODO: Remove hardcoded indices from state
-        self._joint_idx = [i for i in range(self._model['nq']) if i not in self._hyperparams['frozen_joints']]
-        self._vel_idx = [i + self._model['nq'] for i in self._joint_idx]
+        self._joint_idx = [i for i in range(self._model[0]['nq']) if i not in self._hyperparams['frozen_joints']]
+        self._vel_idx = [i + self._model[0]['nq'] for i in self._joint_idx]
 
-        self.x0 = np.concatenate([x0[self._joint_idx], x0[self._vel_idx], eepts, np.zeros_like(eepts)])
+        self.x0 = []
+        for x0 in self.init_mj_X:
+            self.x0.append(np.concatenate([x0[self._joint_idx], x0[self._vel_idx], eepts, np.zeros_like(eepts)]))
 
-    def sample(self, policy, T, verbose=True):
+    def sample(self, policy, T, condition, verbose=True):
         """
         Runs a trial and constructs and returns a new sample containing information
         about the trial.
@@ -68,62 +94,71 @@ class AgentMuJoCo(Agent):
             T: number of time steps for the trial
             verbose: whether or not to plot the trial
         """
-        new_sample = self._init_sample()  # create new sample, populate first time step
-        mj_X = self.init_mj_X
+        new_sample = self._init_sample(condition)  # create new sample, populate first time step
+        mj_X = self.init_mj_X[condition]
         U = np.zeros([T, self.sample_data.dU])
         noise = generate_noise(T, self.sample_data.dU, smooth=self._hyperparams['smooth_noise'], \
                 var=self._hyperparams['smooth_noise_var'], \
                 renorm=self._hyperparams['smooth_noise_renormalize'])
-        if np.any(self._hyperparams['x0var'] > 0):
+        if np.any(self._hyperparams['x0var'][condition] > 0):
             x0n = self._hyperparams['x0var'] * np.random.randn(self._hyperparams['x0var'].shape)
             mj_X += x0n
+        noisy_body_idx = self._hyperparams['noisy_body_idx'][condition]
+        if noisy_body_idx.size > 0:
+            for i in range(len(noisy_body_idx)):
+                idx = noisy_body_idx[i]
+                var = self._hyperparams['noisy_body_var'][condition][i]
+                self._model[condition]['body_pos'][idx,:] += (var * np.random.randn(1,3))
+        self._world.set_model(self._model[condition])
         #TODO: add noise to body pos and then setmodel
         for t in range(T):
             X_t = new_sample.get_X(t=t)
             obs_t = new_sample.get_obs(t=t)
             mj_U = policy.act(X_t, obs_t, t, noise[t,:])
             U[t,:] = mj_U
+            mj_U = np.concatenate([mj_U, np.zeros(len(self._hyperparams['frozen_joints']))])
             if verbose:
-                self._world.Plot(mj_X)
+                self._world.plot(mj_X)
             if (t+1) < T:
-                mj_X, _ = self._world.Step(mj_X, mj_U)
+                for step in range(self._hyperparams['substeps']):
+                    mj_X, _ = self._world.step(mj_X, mj_U)
                 #TODO: update hidden state
-                self._data = self._world.GetData()
-                self._set_sample(new_sample, mj_X, t)
+                self._data = self._world.get_data()
+                self._set_sample(new_sample, mj_X, t, condition)
         #TODO: reset world?
         new_sample.set(Action, U)
         return new_sample
 
-    def _init_sample(self):
+    def _init_sample(self, condition):
         """
         Construct a new sample and fill in the first time step.
         """
         sample = self.sample_data.create_new()
-        sample.set(JointAngles, self.x0[self._joint_idx], t=0)
-        sample.set(JointVelocities, self.x0[self._vel_idx], t=0)
-        self._data = self._world.GetData()
+        sample.set(JointAngles, self.init_mj_X[condition][self._joint_idx], t=0)
+        sample.set(JointVelocities, self.init_mj_X[condition][self._vel_idx], t=0)
+        self._data = self._world.get_data()
         eepts = self._data['site_xpos'].flatten()
         sample.set(EndEffectorPoints, eepts, t=0)
         sample.set(EndEffectorPointVelocities, np.zeros_like(eepts), t=0)
-        jac = np.zeros([eepts.shape[0], self.x0.shape[0]])
+        jac = np.zeros([eepts.shape[0], self.x0[condition].shape[0]])
         for site in range(eepts.shape[0] // 3):
             idx = site * 3
-            jac[idx:(idx+3), range(self._model['nq'])] = self._world.GetJacSite(site)
+            jac[idx:(idx+3), range(self._model[condition]['nq'])] = self._world.get_jac_site(site)
         sample.set(EndEffectorJacobians, jac, t=0)
         return sample
 
-    def _set_sample(self, sample, mj_X, t):
-        sample.set(JointAngles, np.array([mj_X[i] for i in self._joint_idx]), t=t+1)
-        sample.set(JointVelocities, np.array([mj_X[i] for i in self._vel_idx]), t=t+1)
+    def _set_sample(self, sample, mj_X, t, condition):
+        sample.set(JointAngles, np.array(mj_X[self._joint_idx]), t=t+1)
+        sample.set(JointVelocities, np.array(mj_X[self._vel_idx]), t=t+1)
         curr_eepts = self._data['site_xpos'].flatten()
         sample.set(EndEffectorPoints, curr_eepts, t=t+1)
         prev_eepts = sample.get(EndEffectorPoints, t=t)
         eept_vels = (curr_eepts - prev_eepts) / self._hyperparams['dt']
         sample.set(EndEffectorPointVelocities, eept_vels, t=t+1)
-        jac = np.zeros([curr_eepts.shape[0], self.x0.shape[0]])
+        jac = np.zeros([curr_eepts.shape[0], self.x0[condition].shape[0]])
         for site in range(curr_eepts.shape[0] // 3):
             idx = site * 3
-            jac[idx:(idx+3), range(self._model['nq'])] = self._world.GetJacSite(site)
+            jac[idx:(idx+3), range(self._model[condition]['nq'])] = self._world.get_jac_site(site)
         sample.set(EndEffectorJacobians, jac, t=t+1)
 
     def reset(self, condition):
