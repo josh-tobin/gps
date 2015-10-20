593aa4363e80343b44c6e6510ccf62e610517bfb
Xin-Yu-Tan <xinyutan@berkeley.edu>
2015-10-19 21:36:29 -0700

    added initial use of subplotspec

--- python/gps/gui/gui.py
+++ python/gps/gui/gui.py
@@ -1,10 +1,10 @@
+# from gps.gui.config import target_setup
+import copy 
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Button, RadioButtons, Slider
-import inspect
-
-from config import target_setup
-import copy 
+import matplotlib.gridspec as gridspec
+from matplotlib.widgets import Button, RadioButtons, CheckButtons, Slider
 
 # GUI includes:
 # Target setup (responsive to keyboard, gui, and PS3 controller)
@@ -27,49 +27,54 @@ class GUI:
 		config.update(hyperparams)
 		self._hyperparams = config
 
-		
-
 		# GUI components
-		self.fig, self.axarr = plt.subplots(num_functions)
-		
-		
+		self.fig = plt.figure(figsize=(8, 8))
+		self.gs = gridspec.GridSpec(2, 1, height_ratios=[1, 3])
+		num_functions = 10
+		self.gs0 = gridspec.GridSpecFromSubsplotSpec(1, num_functions, subplot_spec=gs[0])
+		self.axarr = [fig.subplot(gs[0, i]) for i in range(num_functions))]
 
-class TargetSetup:
-	def __init__(self, agent, hyperparams):
-		self._agent = agent
-		config = copy.deepcopy(target_setup)
-		config.update(hyperparams)
-		self._hyperparams = config
+		self.b1 = Button(self.ax1, "set_target_number")
+		self.b2 = Button(self.ax1, "set_arm")
 
-		# Target Setup
-		self.arm = 'right_arm'
-		self.target_number = 1
+		self.gs1 = gridspec.GridSpecFromSubsplotSpec(1, 2, subplot_spec=gs[0], width_ratios=[3, 1])
 
-		# GUI figure
-		self.num_functions = 8		# this number is hardcoded
-		self.fig, self.axarr = plt.subplots(num_functions)
-		self.buttons = [None for i in range(num_functions)]
+# class TargetSetup:
+# 	def __init__(self, agent, hyperparams):
+# 		self._agent = agent
+# 		config = copy.deepcopy(target_setup)
+# 		config.update(hyperparams)
+# 		self._hyperparams = config
 
-		self.b1 = Button(axarr[0], 'Set Target Number')
-		self.b1.on_clicked(set_target_number)
-		self.c1 = CheckButtons([i for i in range(1, 14)])
+# 		# Target Setup
+# 		self.arm = 'right_arm'
+# 		self.target_number = 1
 
-		rax = plt.axes([0.05, 0.4, 0.1, 0.15])
-		check = CheckButtons(rax, ('2 Hz', '4 Hz', '6 Hz'), (False, True, True))
+#     # GUI figure
+#     self.num_functions = 8		# this number is hardcoded
+#     self.fig, self.axarr = plt.subplots(num_functions)
+#     self.buttons = [None for i in range(num_functions)]
 
-		def func(label):
-		    if label == '2 Hz': l0.set_visible(not l0.get_visible())
-		    elif label == '4 Hz': l1.set_visible(not l1.get_visible())
-		    elif label == '6 Hz': l2.set_visible(not l2.get_visible())
-		    plt.draw()
-		check.on_clicked(func)
+#     self.b1 = Button(axarr[0], 'Set Target Number')
+#     self.b1.on_clicked(set_target_number)
+#     self.c1 = CheckButtons([i for i in range(1, 14)])
 
-		plt.show()
+#     rax = plt.axes([0.05, 0.4, 0.1, 0.15])
+#     check = CheckButtons(rax, ('2 Hz', '4 Hz', '6 Hz'), (False, True, True))
+
+#     def func(label):
+#       if label == '2 Hz': l0.set_visible(not l0.get_visible())
+#       elif label == '4 Hz': l1.set_visible(not l1.get_visible())
+#       elif label == '6 Hz': l2.set_visible(not l2.get_visible())
+#       plt.draw()
+#     check.on_clicked(func)
+
+#     plt.show()
 
 
 
 
-# TARGET SETUP FUNCTIONS
+	# TARGET SETUP FUNCTIONS
 	def set_target_number(self, event):
 		pass
 
@@ -114,3 +119,6 @@ class TargetSetup:
 			x0 = f['x0']
 		move_arm(self.arm, x0)
 
+if __name__ == "__main__":
+	g = GUI(None, None)
+	plot.show()
