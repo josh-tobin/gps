bb6e4996e6e544960dd085acfeb415bd088f68e2
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2015-10-27 17:50:48 -0700

    added neural network object and it compiles :)

---
+++ src/gps_agent_pkg/include/gps_agent_pkg/neuralnetwork.h
@@ -0,0 +1,45 @@
+/*
+This is the base class for the neural network object. The neural network object
+is a helper class for running neural networks on the robot.
+*/
+#pragma once
+
+// Headers
+#include <Eigen/Dense>
+#include <vector>
+#include <ros/ros.h>
+
+namespace gps_control
+{
+
+// Forward declaration.
+//class NeuralNetwork
+
+class NeuralNetwork {
+protected:
+    // Internal scales and biases.
+    Eigen::MatrixXd scale_;
+    Eigen::VectorXd bias_;
+
+public:
+    bool scale_bias_set_, weights_set_;
+    // Constructor -- doesn't do much, since most of the action is in setting parameters and scales & biases.
+    NeuralNetwork(int num_layers, std::vector<int> link_functions, std::vector<int> layer_size, int output_dims, const std::vector<double> data);
+
+    virtual ~NeuralNetwork();
+
+    // Function that takes in an input state and outputs the neural network output action.
+    // We will likely want our neural network functionality to be more general than this,
+    // but this is the most basic functionality.
+    // This should be implemented in the subclass.
+    virtual void forward(const Eigen::VectorXd &input, Eigen::VectorXd &output);
+
+    // Set the scales and biases, also preallocate any internal temporaries for fast evaluation.
+    // This is implemented in neural_network.cpp
+    virtual void set_scalebias(const std::vector<double> data, const std::vector<int> dims);
+
+    // Set the weights of the neural network (and create it if necessary)
+    virtual void set_weights(void *weights_ptr);
+};
+
+}
---
+++ src/gps_agent_pkg/src/neuralnetwork.cpp
@@ -0,0 +1,61 @@
+#include "gps_agent_pkg/neuralnetwork.h"
+
+using namespace gps_control;
+
+// Initialize neural network from message data.
+NeuralNetwork::NeuralNetwork(int num_layers, std::vector<int> link_functions, std::vector<int> layer_size, int output_dims, const std::vector<double> data)
+{
+  // Nothing to do here.
+}
+
+// Destruct all objects.
+NeuralNetwork::~NeuralNetwork()
+{
+    // Nothing to do here.
+}
+
+void NeuralNetwork::forward(const Eigen::VectorXd &input, Eigen::VectorXd &output)
+{
+    // Nothing to do here.
+}
+
+// Set the scales and biases, also preallocate any internal temporaries for fast evaluation.
+void NeuralNetwork::set_scalebias(const std::vector<double> data, const std::vector<int> dims)
+{
+    // Note that we expect there to be two things in data:
+    // 0: number of entries in scaling matrix
+    // 1: number of entries in bias vector
+
+    // Check validity.
+    assert(dims[0] == dims[1]*dims[1]);
+
+    // Initialize readback index.
+    int idx = 0;
+
+    // Unpack the scaling matrix (stored in column major order).
+    scale_.resize(dims[1],dims[1]);
+    for (int j = 0; j < dims[1]; ++j)
+    {
+        for (int i = 0; i < dims[1]; ++i)
+        {
+            scale_(i,j) = data[idx];
+            idx++;
+        }
+    }
+
+    // Unpack the bias vector.
+    bias_.resize(dims[1]);
+    for (int i = 0; i < dims[1]; ++i)
+    {
+        bias_(i) = data[idx];
+        idx++;
+    }
+
+    ROS_INFO("Scale and bias set successfully");
+    scale_bias_set_ = true;
+}
+
+void NeuralNetwork::set_weights(void *weights_ptr)
+{
+    // Nothing to do here.
+}
