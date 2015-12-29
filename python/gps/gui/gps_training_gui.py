34ec14ed481bcbf993527434ec30d33ce281659a
Xin Yu Tan <xinyutan@berkeley.edu>
2015-12-28 20:41:38 -0800

    changed GPSTrainingGUI to use a MeanPlotter to plot cost (untested)

--- python/gps/gui/gps_training_gui.py
+++ python/gps/gui/gps_training_gui.py
@@ -12,7 +12,7 @@ from gps.gui.config import gps_training as gps_training_config
 from gps.gui.action import Action
 from gps.gui.action_axis import ActionAxis
 from gps.gui.output_axis import OutputAxis
-from gps.gui.real_time_plotter import RealTimePlotter
+from gps.gui.mean_plotter import MeanPlotter
 from gps.gui.image_visualizer import ImageVisualizer
 
 # ~~~ GUI Specifications ~~~
@@ -80,7 +80,7 @@ class GPSTrainingGUI:
         # Plot Axis
         self._gs_plot = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=self._gs[2:4, 0:2])
         self._ax_plot = plt.subplot(self._gs_plot[0])
-        self._plot_axis = RealTimePlotter(self._ax_plot, labels=['cost'])
+        self._plot_axis = MeanPlotter(self._ax_plot, label=['cost'])
 
         # Image Axis
         self._gs_image = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=self._gs[2:4, 2:4])
@@ -126,6 +126,9 @@ class GPSTrainingGUI:
         self._output_axis.append_text(text)
 
     def update(self, algorithm):
-        for t in range(algorithm.T):
-            # update with mean cost over all samples, for each condition m, for each time step t
-            self._plot_axis.update([np.mean(algorithm.prev[m].cs[:, t]) for m in range(algorithm.M)])
+        if algorithm.M == 1:
+            # update with each sample's cost (summed over time)
+            self._plot_axis.update(np.sum(algorithm.prev[0].cs, axis=1))
+        else:
+            # update with each condition's mean sample cost (summed over time)
+            self._plot_axis.update([np.mean(np.sum(algorithm.prev[m].cs, axis=1)) for m in range(algorithm.M)])
