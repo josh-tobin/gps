d9dc9295a9a332d755f3591ba20be8d688fac9ef
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2016-01-19 17:32:22 -0800

    clean up some comments

--- src/gps_agent_pkg/src/caffenncontroller.cpp
+++ src/gps_agent_pkg/src/caffenncontroller.cpp
@@ -1,5 +1,4 @@
 #include "caffe/caffe.hpp"
-#include "google/protobuf/text_format.h"
 
 #include "gps_agent_pkg/caffenncontroller.h"
 #include "gps_agent_pkg/robotplugin.h"
@@ -31,16 +30,10 @@ void CaffeNNController::configure_controller(OptionsMap &options)
     //Call superclass
     TrialController::configure_controller(options);
 
-    //weights_string_ = boost::get<string>(options["weights_string"]);
-    //ROS_INFO(options["net_param"]);
-    ROS_INFO("doing boost get");
     std::string net_param_string = boost::get<string>(options["net_param"]);
-    ROS_INFO("did boost get");
 
     NetParameter net_param;
     net_param.ParseFromString(net_param_string);
-    //google::protobuf::TextFormat::ParseFromString(net_param_string, &net_param);
-    //google::protobuf::MessageLite::ParseFromString(net_param_string, &net_param);
     net_.reset(new NeuralNetworkCaffe(net_param));
     net_->set_weights(net_param);
 
