29bfb365d8890bea0359cea829c5e5ae9b02abe2
Marvin Zhang <zhangmarvin95@gmail.com>
2016-01-25 15:55:27 -0800

    clean up dynamics code

--- python/gps/algorithm/dynamics/config.py
+++ python/gps/algorithm/dynamics/config.py
@@ -1,5 +1,8 @@
-""" DynamicsPriorGMM """
-dyn_prior_gmm = {
+""" Default configuration and hyperparameter values for dynamics objects. """
+
+
+# DynamicsPriorGMM
+DYN_PRIOR_GMM = {
     'min_samples_per_cluster': 20,
     'max_clusters': 50,
     'max_samples': 20,
--- python/gps/algorithm/dynamics/dynamics_utils.py
+++ python/gps/algorithm/dynamics/dynamics_utils.py
@@ -1,11 +1,12 @@
+""" This file defines utility classes and functions for dynamics. """
 import numpy as np
 
 
 def guess_dynamics(gains, acc, dX, dU, dt):
     """
     Initial guess at the model using position-velocity assumption.
-    Note: This code assumes joint positions occupy the first dU state indices and joint velocities
-    occupy the next dU.
+    Note: This code assumes joint positions occupy the first dU state
+          indices and joint velocities occupy the next dU.
     Args:
         gains: dU dimensional joint gains.
         acc: dU dimensional joint acceleration.
@@ -16,11 +17,18 @@ def guess_dynamics(gains, acc, dX, dU, dt):
         Fd: A dX by dX+dU transition matrix.
         fc: A dX bias vector.
     """
-    #TODO: Use packing instead of assuming which indices are the joint angles.
+    #TODO: Use packing instead of assuming which indices are the joint
+    #      angles.
     Fd = np.vstack([
-        np.hstack([np.eye(dU), dt * np.eye(dU), np.zeros((dU, dX-dU*2)), dt ** 2 * np.diag(gains)]),
-        np.hstack([np.zeros((dU, dU)), np.eye(dU), np.zeros((dU, dX-dU*2)), dt * np.diag(gains)]),
-        np.zeros((dX-dU*2, dX+dU))
+        np.hstack([
+            np.eye(dU), dt * np.eye(dU), np.zeros((dU, dX - dU*2)),
+            dt ** 2 * np.diag(gains)
+        ]),
+        np.hstack([
+            np.zeros((dU, dU)), np.eye(dU), np.zeros((dU, dX - dU*2)),
+            dt * np.diag(gains)
+        ]),
+        np.zeros((dX - dU*2, dX+dU))
     ])
-    fc = np.hstack([acc * dt ** 2, acc * dt, np.zeros((dX-dU*2))])
+    fc = np.hstack([acc * dt ** 2, acc * dt, np.zeros((dX - dU*2))])
     return Fd, fc
