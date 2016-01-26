5f3d90313f78072a509ba0c2017344413897d893
Emily Scharff <emily@lfd3.(none)>
2016-01-25 17:57:09 -0800

    Added box2d crawler_world

--- python/gps/agent/box2d/crawler_world.py
+++ python/gps/agent/box2d/crawler_world.py
@@ -7,9 +7,10 @@ import numpy as np
 # Original inspired by a contribution by roman_m
 # Dimensions scooped from APE (http://www.cove.org/ape/index.htm)
 class Crawler (Framework):
-    def __init__(self):
+    def __init__(self, x0=[0], target=[10]):
         super(Crawler, self).__init__()
-
+       
+        self.x0 = x0
         ground = self.world.CreateStaticBody(
                 shapes=[ 
                         b2EdgeShape(vertices=[(-50,0),(50,0)]),
@@ -17,41 +18,30 @@ class Crawler (Framework):
                         b2EdgeShape(vertices=[(50,0),(50,10)]),
                     ]
                 ) 
-        
-        self.ground_level = 1.01499
-
-        boxFixture=b2FixtureDef(
-                shape=b2PolygonShape(box=(0.5,0.5)),
-                density=1,
+        self.ground_level = 1.01499        
+        rectangle_fixture=b2FixtureDef(
+                shape=b2PolygonShape(box=(3.0,0.5)),
+                density=1.5,
                 friction=0.3)
 
-        square_fixture=b2FixtureDef(
-                shape=b2PolygonShape(box=(2.0,1)),
-                density=1,
-                friction=0.3,
-                groupIndex=-1)
-
-        self.square = self.world.CreateDynamicBody(
-                fixtures=square_fixture,
-                position=((0, self.ground_level)))
-
-        circle=b2FixtureDef(
-                shape=b2CircleShape(radius=0.5),
-                density=1,
-                friction=0.3)
-        self.body1=self.world.CreateDynamicBody(
-                position=(5, 0.1),
-                fixtures=boxFixture
+        self.body1 = self.world.CreateDynamicBody(
+                fixtures=rectangle_fixture,
+                position=((x0[0], self.ground_level)),
+                angle=.25*b2_pi
+                )
+        self.body2=self.world.CreateDynamicBody(
+                fixtures=rectangle_fixture,
+                position=(x0[0] + 3, self.ground_level),
+                angle=-.25*b2_pi
                 )
         self.joint=self.world.CreateRevoluteJoint(
                 bodyA=self.body1,
                 bodyB=self.square,
-                anchor=((self.square.position[0] + self.body1.position[0])/2, 5), 
+                localAnchorA=(2.5,0),
+                localAnchorB=(-2.5,0)
                 enableMotor = True,
                 maxMotorTorque=400,
                 )
-        print(self.square.angle)
-
     def Step(self, settings, action):
         self.body.motorSpeed = action[0]
 
@@ -60,18 +50,18 @@ class Crawler (Framework):
     def reset_world(self):
         self.world.ClearForces()
         self.joint.motorSpeed = 0
-        self.square.linearVelocity= (0, 0)
-        self.square.angularVelocity= 0
-        self.body1.linearVelocity = (0, 0)
-        self.body1.angularVelocity = 0
-        self.body1.position = (5, 0.1)
-        self.body1.angle = 0
-        self.square.position = (0, self.ground_level)
-        self.square.angle = 0
+        self.body1.linearVelocity= (0, 0)
+        self.body1.angularVelocity= 0
+        self.body2.linearVelocity = (0, 0)
+        self.body2.angularVelocity = 0
+        self.body1.position = (self.x0[0], self.ground_level),
+        self.body1.angle = .25*b2_pi
+        self.body2.position = (self.x0[0] + 3, self.ground_level)
+        self.body2.angle = -.25*b2_pi
 
 
     def get_state(self):
-        state = {POSITION : np.array(self.square.position), LINEAR_VELOCITY : np.array(self.square.linearVelocity)}
+        state = {POSITION : np.array(self.body1.position), JOINT_ANGLES : np.array(self.joint.angle)}
 
         return state
     
