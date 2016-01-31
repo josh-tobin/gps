5f4e419df840f8c6cba3920d4391de3649e9049d
Marvin Zhang <zhangmarvin95@gmail.com>
2016-01-30 22:21:43 -0800

    clean up agent files

--- python/gps/agent/agent_utils.py
+++ python/gps/agent/agent_utils.py
@@ -1,24 +1,28 @@
+""" This file defines utility classes and functions for agents. """
 import numpy as np
 import scipy.ndimage as sp_ndimage
 
 
 def generate_noise(T, dU, smooth=False, var=1.0, renorm=False):
     """
-    Generate a T x dU gaussian-distributed noise vector. This will approximately have mean 0 and
-    variance 1, ignoring smoothing.
+    Generate a T x dU gaussian-distributed noise vector. This will
+    approximately have mean 0 and variance 1, ignoring smoothing.
 
     Args:
         T: Number of time steps.
         dU: Dimensionality of actions.
         smooth: Whether or not to perform smoothing of noise.
-        var : If smooth=True, applies a Gaussian filter with this variance.
-        renorm : If smooth=True, renormalizes data to have variance 1 after smoothing.
+        var : If smooth=True, applies a Gaussian filter with this
+            variance.
+        renorm : If smooth=True, renormalizes data to have variance 1
+            after smoothing.
     """
     noise = np.random.randn(T, dU)
     if smooth:
-        # Smooth noise. This violates the controller assumption, but might produce smoother motions.
+        # Smooth noise. This violates the controller assumption, but
+        # might produce smoother motions.
         for i in range(dU):
-            noise[:,i] = sp_ndimage.filters.gaussian_filter(noise[:,i], var)
+            noise[:, i] = sp_ndimage.filters.gaussian_filter(noise[:, i], var)
         if renorm:
             variance = np.var(noise, axis=0)
             noise = noise / np.sqrt(variance)
--- python/gps/agent/mjc/agent_mjc.py
+++ python/gps/agent/mjc/agent_mjc.py
@@ -1,21 +1,25 @@
+""" This file defines an agent for the MuJoCo simulator environment. """
 import copy
+
 import mjcpy
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
 
 from gps.agent.agent import Agent
 from gps.agent.agent_utils import generate_noise
-from gps.agent.config import agent_mujoco
-from gps.proto.gps_pb2 import *
+from gps.agent.config import AGENT_MUJOCO
+from gps.proto.gps_pb2 import JOINT_ANGLES, JOINT_VELOCITIES, \
+        END_EFFECTOR_POINTS, END_EFFECTOR_POINT_VELOCITIES, \
+        END_EFFECTOR_POINT_JACOBIANS
 from gps.sample.sample import Sample
 
 
 class AgentMuJoCo(Agent):
     """
-    All communication between the algorithms and MuJoCo is done through this class.
+    All communication between the algorithms and MuJoCo is done through
+    this class.
     """
     def __init__(self, hyperparams):
-        config = copy.deepcopy(agent_mujoco)
+        config = copy.deepcopy(AGENT_MUJOCO)
         config.update(hyperparams)
         Agent.__init__(self, config)
         self._setup_conditions()
@@ -23,20 +27,23 @@ class AgentMuJoCo(Agent):
 
     def _setup_conditions(self):
         """
-        Helper method for setting some hyperparameters that may vary by condition.
+        Helper method for setting some hyperparameters that may vary by
+        condition.
         """
         def setup(value, n):
+            """ Go through various types of hyperparameters. """
             if not isinstance(value, list):
                 try:
                     return [value.copy() for _ in range(n)]
                 except AttributeError:
                     return [value for _ in range(n)]
-            assert len(value) == n, 'Number of elements must match number of conditions.'
+            assert len(value) == n, \
+                    'Number of elements must match number of conditions.'
             return value
 
         conds = self._hyperparams['conditions']
-        for field in ('x0', 'x0var', 'pos_body_idx', 'pos_body_offset', 'noisy_body_idx',
-                'noisy_body_var'):
+        for field in ('x0', 'x0var', 'pos_body_idx', 'pos_body_offset',
+                      'noisy_body_idx', 'noisy_body_var'):
             self._hyperparams[field] = setup(self._hyperparams[field], conds)
 
     def _setup_world(self, filename):
@@ -57,13 +64,15 @@ class AgentMuJoCo(Agent):
         self._world.set_option(options)
 
         self._model = self._world.get_model()
-        self._model = [copy.deepcopy(self._model) for _ in range(self._hyperparams['conditions'])]
+        self._model = [copy.deepcopy(self._model)
+                       for _ in range(self._hyperparams['conditions'])]
         self._options = self._world.get_option()
 
         for i in range(self._hyperparams['conditions']):
             for j in range(len(self._hyperparams['pos_body_idx'][i])):
                 idx = self._hyperparams['pos_body_idx'][i][j]
-                self._model[i]['body_pos'][idx,:] += self._hyperparams['pos_body_offset'][i]
+                self._model[i]['body_pos'][idx, :] += \
+                        self._hyperparams['pos_body_offset'][i]
             self._world.set_model(self._model[i])
             x0 = self._hyperparams['x0'][i]
             idx = len(x0) // 2
@@ -81,44 +90,52 @@ class AgentMuJoCo(Agent):
         self.x0 = []
         for x0 in self._hyperparams['x0']:
             if END_EFFECTOR_POINTS in self.x_data_types:
-                self.x0.append(np.concatenate([x0, eepts, np.zeros_like(eepts)]))
+                self.x0.append(
+                    np.concatenate([x0, eepts, np.zeros_like(eepts)])
+                )
             else:
                 self.x0.append(x0)
 
     def sample(self, policy, condition, verbose=True, save=True):
         """
-        Runs a trial and constructs a new sample containing information about the trial.
+        Runs a trial and constructs a new sample containing information
+        about the trial.
         Args:
             policy: Policy to to used in the trial.
             condition: Which condition setup to run.
             verbose: Whether or not to plot the trial.
-            save: Whether or not to store the trial into the list of samples.
+            save: Whether or not to store the trial into the samples.
         """
-        new_sample = self._init_sample(condition)  # Create new sample, populate first time step.
+        # Create new sample, populate first time step.
+        new_sample = self._init_sample(condition)
         mj_X = self._hyperparams['x0'][condition]
         U = np.zeros([self.T, self.dU])
-        noise = generate_noise(self.T, self.dU, smooth=self._hyperparams['smooth_noise'],
-                var=self._hyperparams['smooth_noise_var'],
-                renorm=self._hyperparams['smooth_noise_renormalize'])
+        noise = generate_noise(
+            self.T, self.dU, smooth=self._hyperparams['smooth_noise'],
+            var=self._hyperparams['smooth_noise_var'],
+            renorm=self._hyperparams['smooth_noise_renormalize']
+        )
         if np.any(self._hyperparams['x0var'][condition] > 0):
-            x0n = self._hyperparams['x0var'] * np.random.randn(self._hyperparams['x0var'].shape)
+            x0n = self._hyperparams['x0var'] * \
+                    np.random.randn(self._hyperparams['x0var'].shape)
             mj_X += x0n
         noisy_body_idx = self._hyperparams['noisy_body_idx'][condition]
         if noisy_body_idx.size > 0:
             for i in range(len(noisy_body_idx)):
                 idx = noisy_body_idx[i]
                 var = self._hyperparams['noisy_body_var'][condition][i]
-                self._model[condition]['body_pos'][idx,:] += (var * np.random.randn(1,3))
+                self._model[condition]['body_pos'][idx, :] += \
+                        var * np.random.randn(1, 3)
         self._world.set_model(self._model[condition])
         for t in range(self.T):
             X_t = new_sample.get_X(t=t)
             obs_t = new_sample.get_obs(t=t)
-            mj_U = policy.act(X_t, obs_t, t, noise[t,:])
-            U[t,:] = mj_U
+            mj_U = policy.act(X_t, obs_t, t, noise[t, :])
+            U[t, :] = mj_U
             if verbose:
                 self._world.plot(mj_X)
-            if (t+1) < self.T:
-                for step in range(self._hyperparams['substeps']):
+            if (t + 1) < self.T:
+                for _ in range(self._hyperparams['substeps']):
                     mj_X, _ = self._world.step(mj_X, mj_U)
                 #TODO: Some hidden state stuff will go here.
                 self._data = self._world.get_data()
@@ -134,8 +151,10 @@ class AgentMuJoCo(Agent):
             condition: Which condition to initialize.
         """
         sample = Sample(self)
-        sample.set(JOINT_ANGLES, self._hyperparams['x0'][condition][self._joint_idx], t=0)
-        sample.set(JOINT_VELOCITIES, self._hyperparams['x0'][condition][self._vel_idx], t=0)
+        sample.set(JOINT_ANGLES,
+                   self._hyperparams['x0'][condition][self._joint_idx], t=0)
+        sample.set(JOINT_VELOCITIES,
+                   self._hyperparams['x0'][condition][self._vel_idx], t=0)
         self._data = self._world.get_data()
         eepts = self._data['site_xpos'].flatten()
         sample.set(END_EFFECTOR_POINTS, eepts, t=0)
@@ -143,7 +162,7 @@ class AgentMuJoCo(Agent):
         jac = np.zeros([eepts.shape[0], self._model[condition]['nq']])
         for site in range(eepts.shape[0] // 3):
             idx = site * 3
-            jac[idx:(idx+3),:] = self._world.get_jac_site(site)
+            jac[idx:(idx+3), :] = self._world.get_jac_site(site)
         sample.set(END_EFFECTOR_POINT_JACOBIANS, jac, t=0)
         return sample
 
@@ -166,5 +185,5 @@ class AgentMuJoCo(Agent):
         jac = np.zeros([curr_eepts.shape[0], self._model[condition]['nq']])
         for site in range(curr_eepts.shape[0] // 3):
             idx = site * 3
-            jac[idx:(idx+3),:] = self._world.get_jac_site(site)
+            jac[idx:(idx+3), :] = self._world.get_jac_site(site)
         sample.set(END_EFFECTOR_POINT_JACOBIANS, jac, t=t+1)
