daf2c8e8c558cdde425db071bb13f90f333220f5
Josh Tobin <joshp.tobin@gmail.com>
2016-03-28 10:24:26 -0700

    change to goto script

--- goto.sh
+++ goto.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rostopic pub -1 /gps_controller_position_command gps_agent_pkg/PositionCommand '{id: 0, mode: 1, arm: 0, data: [0,0.5,0,0,0,0,0], pd_gains: [3.09, 1.08, 0.393, 0.674, 0.111, 0.152, 0.098]}'
-
+rostopic pub /gps_controller_position_command gps_agent_pkg/PositionCommand '{id: 0, mode: 1, arm: 0, data: [0.1, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0], pd_gains: [3.09, 1.08, 0.393, 0.674, 0.111, 0.152, 0.098]}'
