e78219199dedd85de7ad3c2076296e1bf2ad7dde
Josh Tobin <joshp.tobin@gmail.com>
2016-06-26 13:49:23 -0700

    Latest experiments

--- policy_tester.py
+++ policy_tester.py
@@ -10,10 +10,11 @@ from gps.agent.agent_utils import generate_noise
 from saved_examples.util import PolicyTester
 import argparse
 import os
+import cPickle as pickle
 
 #EXP_FILE = '/home/jt/gps/experiments/'
 EXP_FILE = os.path.dirname(os.path.realpath(__file__))
-EXP_FILE += 'experiments/'
+EXP_FILE += '/experiments/'
 HYPERPARAMS_FILE = '/hyperparams.py'
 POLICY_FILE = '/data_files/'
 
@@ -29,10 +30,18 @@ def main():
     collect_sample(args.hyperparams_file, args.policy_file, args.policy_iter,
                    n_samples=args.n_samples)
 
-
 def collect_sample(exp_name, policy_name, policy_iter, n_samples=5):
     tester = PolicyTester(exp_name)
-    tester.test(policy_name, policy_iter, verbose=True, n_samples=n_samples)
+    err = tester.test(policy_name, policy_iter, verbose=True, n_samples=n_samples)
+    error_data_file = EXP_FILE + policy_name + POLICY_FILE + 'error_data.pkl'
+    try:
+        with open(error_data_file, 'r') as f:
+            err_data = pickle.load(f)
+    except:
+        err_data = {'train_error': {}, 'test_error': {}}
+    err_data['test_error'][exp_name] = err
+    with open(error_data_file, 'wb') as f:
+        pickle.dump(err_data, f)
 
 if __name__ == '__main__':
     main()
---
+++ print_results.py
@@ -0,0 +1,35 @@
+import cPickle as pickle
+import numpy as np
+import argparse
+
+DATA_LOCATION = '/home/jt/gps/experiments/'
+
+def print_exp(exp_name, exp_data):
+    print '------------------------------'
+    print 'Experiment %s'%exp_name
+    print
+    
+    if exp_data['train_error']:
+        print 'Train error:'
+        print '\t mean: %.2f \t std: %.2f'%(
+            np.mean(exp_data['train_error'][max_iter]),
+            np.std(exp_data['train_error'][max_iter]))
+        print
+    for key, value in exp_data['test_error'].iteritems():
+        print '\t Evaluated on %s:'%key
+        print '\t ',
+        for member in value:
+            print "%.2f "%member,
+        print
+        print
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment_name')
+    args = parser.parse_args()
+    with open(DATA_LOCATION + args.experiment_name + '/data_files/error_data.pkl', 'r') as f:
+        data = pickle.load(f)
+    print_exp(args.experiment_name, data)
+
+if __name__ == '__main__': 
+    main()
