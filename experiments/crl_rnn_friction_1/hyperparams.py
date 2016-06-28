2a4b044ef8254c2802e422f6f895674aab4da200
Josh Tobin <joshp.tobin@gmail.com>
2016-06-28 10:54:26 -0700

    Experiments fm last night

---
+++ experiments/crl_rnn_friction_1/hyperparams.py
@@ -0,0 +1,307 @@
+""" Hyperparameters for MJC peg insertion policy optimization. """
+from __future__ import division
+
+from datetime import datetime
+import os.path
+
+import numpy as np
+
+from gps import __file__ as gps_filepath
+from gps.agent.mjc.agent_mjc import AgentMuJoCo
+from gps.algorithm.algorithm_badmm import AlgorithmBADMM
+from gps.algorithm.cost.cost_fk import CostFK
+from gps.algorithm.cost.cost_action import CostAction
+from gps.algorithm.cost.cost_sum import CostSum
+from gps.algorithm.cost.cost_utils import RAMP_FINAL_ONLY, RAMP_LINEAR
+from gps.algorithm.dynamics.dynamics_lr_prior import DynamicsLRPrior
+from gps.algorithm.dynamics.dynamics_prior_gmm import DynamicsPriorGMM
+from gps.algorithm.traj_opt.traj_opt_lqr_python import TrajOptLQRPython
+#from gps.algorithm.policy_opt.policy_opt_caffe import PolicyOptCaffe
+from gps.algorithm.policy_opt.policy_opt_tf import PolicyOptTf
+from gps.algorithm.policy.lin_gauss_init import init_lqr
+from gps.algorithm.policy.policy_prior_gmm import PolicyPriorGMM
+from gps.algorithm.policy.policy_prior import PolicyPrior
+from gps.proto.gps_pb2 import JOINT_ANGLES, JOINT_VELOCITIES, \
+        END_EFFECTOR_POINTS, END_EFFECTOR_POINT_VELOCITIES, ACTION
+from gps.gui.config import generate_experiment_info
+from gps.algorithm.policy_opt.tf_model_example import example_tf_network
+from gps.utility.general_utils import get_ee_points
+from gps.gui.target_setup_gui import load_pose_from_npz
+import itertools
+from gps.utility.general_utils import find_bodies_in_model
+
+from gps.algorithm.policy_opt.tf_rnn import crl_rnn_network, crl_rnn_large, CRLRNN
+
+
+BASE_DIR = '/'.join(str.split(gps_filepath, '/')[:-2])
+
+#############
+# TO CHANGE #
+#############
+
+SAMPLE_STRATEGY = 'uniform'
+MASS_STD = 0.25
+MASS_RANGE = np.array([1.0, 1.0])
+GAIN_RANGE = np.array([1.0, 1.0])
+DAMPING_RANGE = np.array([1.0, 1.0])
+FRICTION_RANGE = np.array([0.0, 1.0])
+
+EXP_DIR = BASE_DIR + '/../experiments/crl_rnn_friction_1/'
+BATCH_SIZE = 10
+N_EPOCHS = 350
+MODEL = CRLRNN(batch_size=BATCH_SIZE, n_steps = 100,
+               rnn_output_dim=64, hidden_dim=32, n_feedforward=3,
+               n_recurrent=3)
+
+CONDITIONS = 16
+SAMPLES_PER_CONDITION = 10
+
+###############
+# DONT CHANGE #
+###############
+
+EE_POINTS = np.array([[0.02, -0.025, 0.05], [0.02, -0.025, 0.05],
+                  [0.02, 0.05, 0.0]])
+
+MODEL_PATH = './mjc_models/PR2/pr2_1arm.xml' 
+
+body_names = ['l_shoulder_pan_link', 'l_shoulder_lift_link', 
+              'l_upper_arm_roll_link', 'l_upper_arm_link',
+              'l_elbow_flex_link', 'l_forearm_roll_link',
+              'l_forearm_link']
+
+SENSOR_DIMS = {
+    JOINT_ANGLES: 7,
+    JOINT_VELOCITIES: 7,
+    END_EFFECTOR_POINTS: 3 * EE_POINTS.shape[0],
+    END_EFFECTOR_POINT_VELOCITIES: 3 * EE_POINTS.shape[0],
+    ACTION: 7,
+}
+
+PR2_GAINS = np.array([3.09, 1.08, 0.393, 0.674, 0.111, 0.152, 0.098])
+
+
+x0s = []
+ee_tgts = []
+reset_conditions = []
+
+common = {
+    'experiment_name': 'my_experiment' + '_' + \
+            datetime.strftime(datetime.now(), '%m-%d-%y_%H-%M'),
+    'experiment_dir': EXP_DIR,
+    'data_files_dir': EXP_DIR + 'data_files/',
+    'target_filename': EXP_DIR + 'target.npz',
+    'log_filename': EXP_DIR + 'log.txt',
+    'conditions': CONDITIONS,
+}
+
+#for i in xrange(common['conditions']):
+for i in range(1):
+    ja_x0, ee_pos_x0, ee_rot_x0 = load_pose_from_npz(
+            common['target_filename'], 'trial_arm', str(i), 'initial'
+    )
+    ja_x0_tgt, ee_pos_tgt, ee_rot_tgt = load_pose_from_npz(
+            common['target_filename'], 'trial_arm', str(i), 'target'
+    )
+
+    x0 = np.zeros(32)
+    x0[:7] = ja_x0
+    eepts =  np.ndarray.flatten(
+            get_ee_points(EE_POINTS, ee_pos_x0, ee_rot_x0).T)
+    x0[14:(14+3*EE_POINTS.shape[0])] = eepts
+
+    ee_tgt = np.ndarray.flatten(
+            get_ee_points(EE_POINTS, ee_pos_tgt, ee_rot_tgt).T
+    )
+
+    x0s.append(x0)
+    # For reaching experiment
+    ee_tgts.append(ee_tgt)
+    # For holdarm experiment, tgt is the same as x0
+    #ee_tgts.append(eepts)
+
+
+x0s*= common['conditions']
+ee_tgts *= common['conditions']
+
+
+if not os.path.exists(common['data_files_dir']):
+    os.makedirs(common['data_files_dir'])
+
+bodies = find_bodies_in_model(MODEL_PATH, body_names)
+body_idx = bodies.values()
+num_bodies = len(body_idx)
+
+if SAMPLE_STRATEGY == 'uniform':
+    mass_body_mult = [np.random.uniform(low=MASS_RANGE[0], 
+                                         high=MASS_RANGE[1], size=num_bodies)
+                       for _ in range(common['conditions'])]
+
+    gain_scale = [np.random.uniform(low=GAIN_RANGE[0],
+                                    high=GAIN_RANGE[1], size=7)
+                  for _ in range(common['conditions'])]
+    damping_scale = [np.random.uniform(low=DAMPING_RANGE[0],
+                                       high=DAMPING_RANGE[1], size=(7,1))
+                      for _ in range(common['conditions'])]
+    friction = [np.random.uniform(low=FRICTION_RANGE[0],
+                                  high=FRICTION_RANGE[1],
+                                  size=(7,1))
+                for _ in range(common['conditions'])]
+elif SAMPLE_STRATEGY == 'normal':
+     
+    gain_scale = [np.random.normal(loc=1.0, 
+                                       scale=GAIN_STD, size=7) 
+                      for _ in range(common['conditions'])]
+    mass_body_mult = [np.random.normal(loc=1.0, 
+                                       scale=MASS_STD, size=num_bodies) 
+                      for _ in range(common['conditions'])]
+elif SAMPLE_STRATEGY == 'all':
+    mass_body_mult = [np.ones(num_bodies)]
+    mass_body_mult += list(itertools.product(MASS_RANGE, repeat=num_bodies))
+    gain_scale = [np.ones(7)]*common['conditions']
+
+agent = {
+    'type': AgentMuJoCo,
+    'filename': MODEL_PATH,
+    'x0': [x[:14] for x in x0s],
+    'dt': 0.05,
+    'substeps': 50,
+    'ee_points_tgt': ee_tgts,
+    'conditions': common['conditions'],
+    'mass_body_idx': np.array(body_idx),
+    'mass_body_mult': mass_body_mult,
+    'gain_scale': gain_scale,
+    'damping_mult': damping_scale,
+    'friction': friction,
+    'pos_body_idx': np.array([1]),
+    'pos_body_offset': np.array([0.0, 0.0, 0.0]),
+    'body_color_offset': np.array([1]),
+    'T': 100,
+    'sensor_dims': SENSOR_DIMS,
+    'state_include': [JOINT_ANGLES, JOINT_VELOCITIES, END_EFFECTOR_POINTS,
+                      END_EFFECTOR_POINT_VELOCITIES],
+    'obs_include': [JOINT_ANGLES, JOINT_VELOCITIES, END_EFFECTOR_POINTS,
+                    END_EFFECTOR_POINT_VELOCITIES, ACTION],
+    'camera_pos': np.array([3., 0.5, 4., 0.25, 0, 0.5]),
+}
+
+algorithm = {
+    'type': AlgorithmBADMM,
+    'conditions': common['conditions'],
+    'iterations': 10,
+    'lg_step_schedule': np.array([1e-4, 1e-3, 1e-2, 1e-2]),
+    'policy_dual_rate': 0.2,
+    'ent_reg_schedule': np.array([1e-3, 1e-3, 1e-2, 1e-1]),
+    'fixed_lg_step': 3,
+    'kl_step': 2.0,
+    'min_step_mult': 0.01,
+    'max_step_mult': 1.0,
+    'sample_decrease_var': 0.05,
+    'sample_increase_var': 0.1,
+    'policy_sample_mode': 'replace',
+    'save_samples': False,
+    'save_algoirithm': False,
+    'save_policy': True,
+}
+
+algorithm['init_traj_distr'] = {
+    'type': init_lqr,
+    'init_gains':  1.0 / PR2_GAINS,
+    'init_acc': np.zeros(SENSOR_DIMS[ACTION]),
+    'init_var': 1.0, # vs 1.0 for std experiment
+    'stiffness': 1.0,
+    'stiffness_vel': 0.5,
+    'final_weight': 50.0,
+    'dt': agent['dt'],
+    'T': agent['T'],
+}
+
+torque_cost = {
+    'type': CostAction,
+    'wu': 5e-3 / PR2_GAINS,
+}
+
+ee_pts_tgt = np.zeros([9,])
+fk_cost = {
+    'type': CostFK,
+    'target_end_effector': ee_pts_tgt,
+    'wp': np.ones(3*EE_POINTS.shape[0]),
+    'l1': 0.1,
+    'l2': 1.0, #10.0
+    'alpha': 1e-5,
+    'ramp_option': RAMP_LINEAR,
+}
+
+# Create second cost function for last step only.
+final_cost = {
+    'type': CostFK,
+    'ramp_option': RAMP_FINAL_ONLY,
+    'target_end_effector': fk_cost['target_end_effector'],
+    'wp': fk_cost['wp'],
+    'l1': 1.0,
+    'l2': 0.0,
+    'alpha': 1e-5,
+    'wp_final_multiplier': 10.0,
+}
+
+algorithm['cost'] = {
+    'type': CostSum,
+    'costs': [torque_cost, fk_cost, final_cost],
+    'weights': [1.0, 1.0, 1.0],
+}
+
+algorithm['dynamics'] = {
+    'type': DynamicsLRPrior,
+    'regularization': 1e-6,
+    'prior': {
+        'type': DynamicsPriorGMM,
+        'max_clusters': 20,
+        'min_samples_per_cluster': 40,
+        'max_samples': 20,
+    },
+}
+
+algorithm['traj_opt'] = {
+    'type': TrajOptLQRPython,
+}
+
+#algorithm['policy_opt'] = {
+#    'type': PolicyOptCaffe,
+#    'weights_file_prefix': EXP_DIR + 'policy',
+#    'iterations': 2000,
+#}
+algorithm['policy_opt'] = {
+    'type': PolicyOptTf,
+    'network_params': {
+        'obs_include': [JOINT_ANGLES, JOINT_VELOCITIES, ACTION],
+        'obs_vector_data': [JOINT_ANGLES, JOINT_VELOCITIES, ACTION],
+        'sensor_dims': SENSOR_DIMS,
+    },
+    'recurrent': True,
+    'network_model': MODEL,
+    'iterations': N_EPOCHS,
+    'weights_file_prefix': EXP_DIR + 'policy',
+    'checkpoint_prefix': EXP_DIR + 'data_files/policy_checkpoint.ckpt',
+    'batch_size': BATCH_SIZE,
+    'use_gpu': True,
+}
+
+algorithm['policy_prior'] = {
+    'type': PolicyPriorGMM,
+    'max_clusters': 20,
+    'min_samples_per_cluster': 40,
+    'max_samples': 20,
+}
+
+config = {
+    'iterations': algorithm['iterations'],
+    'num_samples': SAMPLES_PER_CONDITION,
+    'verbose_trials': 1,
+    'verbose_policy_trials': 1,
+    'common': common,
+    'agent': agent,
+    'gui_on': True,
+    'algorithm': algorithm,
+}
+
+common['info'] = generate_experiment_info(config)
